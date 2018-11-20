10dcc7f704a3cc0bc7268a45e0d581d9e19ad349
LadyRavenFire <39091171+LadyRavenFire@users.noreply.github.com>
2018-11-20 09:00:22 +0500

    Code Rewiew

--- Project Aperture/Assets/Scripts/Player/CollectRadioPart.cs
+++ Project Aperture/Assets/Scripts/Player/CollectRadioPart.cs
@@ -14,15 +14,15 @@ public class CollectRadioPart : MonoBehaviour
         _radioPartsHave = 0;
         _radioObjects = GameObject.FindGameObjectsWithTag(RadioTag);
         _radioPartsMax = _radioObjects.Length;
-        print(_radioPartsMax);
+       // print(_radioPartsMax);
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        print("lol1");
+        // print("lol1");
         if (col.gameObject.tag == RadioTag)
         {
-            print("lol2");
+           // print("lol2");
             _radioPartsHave++;
             Destroy(col.gameObject);
         }
