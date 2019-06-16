847d5673c580ee6eea00721f5c7dba0a6e0b8c07
LadyRavenFire <kilkust@gmail.com>
2019-06-16 13:42:49 +0500

    Create pause menu

--- Project Aperture/Assets/Scripts/UI/PlayerParametrs.cs
+++ Project Aperture/Assets/Scripts/UI/PlayerParametrs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerParametrs : MonoBehaviour
@@ -12,18 +13,30 @@ public class PlayerParametrs : MonoBehaviour
     private GameObject _playerPanel;
     private GameObject _winPanel;
     private GameObject _deathPanel;
+    private GameObject _pausePanel;
 
     public Text FoodHave;
     public Text WaterHave;
     public Text RadioPartsHave;
     public Text RadioPartsMax;
 
+    private Button _mainMenuButton;
+    private Button _restartButton;
+
     private CollectRadioPart _collectRadioPart;
     private FoodNeeds _foodNeeds;
     private WaterNeeds _waterNeeds;
 
+    private bool _isGamePaused;
+
     void Start()
     {
+        _mainMenuButton = GameObject.Find("GoToMainMenuButton").GetComponent<Button>();
+        _mainMenuButton.onClick.AddListener(GoToMainMenu);
+
+        _restartButton = GameObject.Find("RestartButton").GetComponent<Button>();
+        _restartButton.onClick.AddListener(RestartLevel);
+
         _player = GameObject.Find(PlayerName);
         _playerPanel = GameObject.Find(PlayerPanelName);
 
@@ -32,6 +45,10 @@ public class PlayerParametrs : MonoBehaviour
 
         _deathPanel = GameObject.Find("DeathPanel");
         _deathPanel.SetActive(false);
+
+        _pausePanel = GameObject.Find("PausePanel");
+        _pausePanel.SetActive(false);
+        _isGamePaused = false;
 
         _collectRadioPart = _player.GetComponent<CollectRadioPart>();
         _foodNeeds = _player.GetComponent<FoodNeeds>();
@@ -45,6 +62,20 @@ public class PlayerParametrs : MonoBehaviour
         UpdateText();
         CheckForWin();
         CheckForDeath();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isGamePaused == false)
+            {
+                _pausePanel.SetActive(true);
+                _isGamePaused = true;
+            }
+            else
+            {
+                _pausePanel.SetActive(false);
+                _isGamePaused = false;
+            }
+        }
     }
 
     void CheckForWin()
@@ -72,6 +103,16 @@ public class PlayerParametrs : MonoBehaviour
         RadioPartsHave.text = "RadioParts: " + _collectRadioPart.ReturnHave();
         FoodHave.text = "Food: " + String.Format("{0:0.00}", _foodNeeds.ReturnFood());
         WaterHave.text = "Water: " + String.Format("{0:0.00}",_waterNeeds.ReturnWater());
+    }
+
+    void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
 
